33b3419dde6b013ad101e8d2441e4d0840ed3b3a
sjcode236 <31339109+sjcode236@users.noreply.github.com>
2018-07-07 23:21:23 -0700

    Update pipCmdList.py

--- pipCmdList.py
+++ pipCmdList.py
@@ -1,6 +1,22 @@
 
 
 ===What is pip
-pip is a package manager for the Python programming language. It provides access to a massive repository of open source tools and libraries, as well as the ability to manage your own proprietary packages. One of the greatest benefits to using a package manager is that it automatically installs the dependencies of any package you install, allowing you to more quickly get back to writing some code.
+What is pip
+pip is a package manager for the Python programming language. It provides access to a massive repository of 
+open source tools and libraries, as well as the ability to manage your own proprietary packages. One of the greatest 
+benefits to using a package manager is that it automatically installs the dependencies of any package you install, 
+allowing you to more quickly get back to writing some code.
 
-
+===Installing pip 
+pip is already installed if you are using Python 2 >=2.7.9 or Python 3 >=3.4 downloaded from python.org 
+pip install instructions are  at  
+https://pip.pypa.io/en/stable/installing/
+  
+===Upgrading pip
+On Linux or macOS:
+    pip install -U pip
+On Windows [4]:
+    python -m pip install -U pip
+    
+ 
+  
