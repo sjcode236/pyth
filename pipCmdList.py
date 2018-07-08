19ccba4869c790f7ea676b24791ad1fa539ae993
sjcode236 <31339109+sjcode236@users.noreply.github.com>
2018-07-07 22:57:47 -0700

    Create pipCmdList.py

---
+++ pipCmdList.py
@@ -0,0 +1,6 @@
+
+
+===What is pip
+pip is a package manager for the Python programming language. It provides access to a massive repository of open source tools and libraries, as well as the ability to manage your own proprietary packages. One of the greatest benefits to using a package manager is that it automatically installs the dependencies of any package you install, allowing you to more quickly get back to writing some code.
+
+
