6e220c95f7c841e8f2d414b5c11caaa2654da9bd
sjcode236 <31339109+sjcode236@users.noreply.github.com>
2018-07-07 17:54:04 -0700

    Update tensorfl1.py

--- tensorfl1.py
+++ tensorfl1.py
@@ -1,11 +1,28 @@
 
-
 anaconda  installation  
 
 download anaconda 
 https://anaconda.org/
 https://www.anaconda.com/download/
-  
+   download Anaconda 5.2 For Windows Installer   Python 3.6 version 
+    
+https://docs.anaconda.com/anaconda/user-guide/getting-started
+
+install anaconda 
+start ->  anaconda   navigator
+=>(click)environments
+=> (select)  not installed   on the right side 
+=>  search for tensorflow , keras , pydot , graphviz   , then select them , then click apply
+
+
+===>  to remove anaconda ,  delete Anaconda3  folder and .anaconda  filder in  C:\Users\Admin 
+
+==>  (at) Home  =>  click Jupytor notebook 
+
+copy the commands at  below link and do a test run 
+http://www.tensorflow.org/get_started/eager
+==>  in jupytor Notebook use   shift-enter  to execute the code 
+
 
 
 
