158524127070d0ed7be77ef56339e5fd12098265
sjcode236 <31339109+sjcode236@users.noreply.github.com>
2018-07-09 00:23:33 -0700

    Update install Keras-tensorflow.py

--- tensorflow/install Keras-tensorflow.py
+++ tensorflow/install Keras-tensorflow.py
@@ -2,6 +2,15 @@
 
 
 https://www.pyimagesearch.com/2016/11/14/installing-keras-with-tensorflow-backend/
+  
+
+$ pip install --upgrade tensorflow
+$ pip install numpy scipy
+$ pip install scikit-learn
+$ pip install pillow
+$ pip install h5py
+$pip install keras
+
 
 
 
